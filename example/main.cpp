bf8f9196c0c31a078426ec323505b09e11eb78fc
Zhalgas <zhalgas99@outlook.com>
2023-06-09 00:41:25 +0300

    fix

--- example/main.cpp
+++ example/main.cpp
@@ -3,5 +3,9 @@
 #include <algo/finite_field/finite_field.hpp>
 
 int main() {
+    for (int i = 0; i < 30; ++i) {
+        algo::ModuloField<31> a {i};
+        std::cerr << a.ToPower(6).value() << std::endl;
+    }
     return 0;
 }
