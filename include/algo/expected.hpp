3eff32a233afe04abf22d13da0b89b9e86b0c8c5
Zhalgas <zhalgas99@outlook.com>
2023-10-11 19:10:14 +0600

    fix style

--- include/algo/expected.hpp
+++ include/algo/expected.hpp
@@ -1,62 +1,59 @@
 #pragma once
 
+#include <system_error>
 #include <type_traits>
 #include <variant>
-#include <system_error>
 
 namespace algo {
 
-    template<typename T, typename E = std::error_condition>
-        requires (
-            !std::is_reference_v<T>
-            && !std::is_same_v<std::remove_cv_t<T>, void>
-            && !std::is_same_v<std::remove_cv_t<T>, std::remove_cv_t<E>>
-        )
-    class Expected : public std::variant<T, E> {
-        using Base = std::variant<T, E>;
+template<typename T, typename E = std::error_condition>
+  requires(!std::is_reference_v<T> &&
+           !std::is_same_v<std::remove_cv_t<T>, void> &&
+           !std::is_same_v<std::remove_cv_t<T>, std::remove_cv_t<E>>)
+class Expected : public std::variant<T, E> {
+  using Base = std::variant<T, E>;
 
-    public:
-        Expected(E ec)
-            : Base{std::move(ec)}
-        {}
+public:
+  Expected(E ec)
+      : Base{std::move(ec)} {
+  }
 
-        template<typename... Args>
-        Expected(Args&&... args)
-            : Base{std::in_place_type<T>, std::forward<Args>(args)...}
-        {}
+  template<typename... Args>
+  Expected(Args&&... args)
+      : Base{std::in_place_type<T>, std::forward<Args>(args)...} {
+  }
 
-        operator bool() const noexcept {
-            return !std::holds_alternative<E>(*this);
-        }
+  operator bool() const noexcept {
+    return !std::holds_alternative<E>(*this);
+  }
 
-        T&& operator*() && noexcept {
-            return std::get<T>(std::move(*this));
-        }
+  T&& operator*() && noexcept {
+    return std::get<T>(std::move(*this));
+  }
 
-        T& operator*() & noexcept {
-            return std::get<T>(*this);
-        }
+  T& operator*() & noexcept {
+    return std::get<T>(*this);
+  }
 
-        const T& operator*() const & noexcept {
-            return std::get<T>(*this);
-        }
+  const T& operator*() const& noexcept {
+    return std::get<T>(*this);
+  }
 
-        T* operator->() noexcept {
-            return &std::get<T>(*this);
-        }
+  T* operator->() noexcept {
+    return &std::get<T>(*this);
+  }
 
-        const T* operator->() const noexcept {
-            return &std::get<T>(*this);
-        }
+  const T* operator->() const noexcept {
+    return &std::get<T>(*this);
+  }
 
-        const E& Error() const noexcept {
-            if (*this) {
-                return E{};
-            }
+  const E& Error() const noexcept {
+    if (*this) {
+      return E{};
+    }
 
-            return std::get<E>(*this);
-        }
-
-    };
+    return std::get<E>(*this);
+  }
+};
 
 } // namespace algo
